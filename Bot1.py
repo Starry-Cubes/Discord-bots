3e38d89f31c02eaaaffbd5170daffff925a077e2
Starry-Cubes <konstancja.konsti@gmail.com>
2025-09-03 20:12:30 +0200

    Create Bot1.py
    actually added a bot

---
+++ Bot1.py
@@ -0,0 +1,33 @@
+#technically Bot2 but we don't talk about that
+
+import discord
+from discord.ext import commands
+from botLogic import gen_pas
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    await ctx.send(left + right)
+
+@bot.command()
+async def genpass(ctx, pass_length = 10):
+    await ctx.send(gen_pas(pass_length))
+
+
+bot.run(token)
